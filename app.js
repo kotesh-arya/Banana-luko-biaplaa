b322447ea10e114248adc62e1982f9d0940d0c94
kotesh-arya <koteswarraomudila@gmail.com>
2020-12-08 22:14:32 +0530

    Except Styles

--- app.js
+++ app.js
@@ -2,8 +2,34 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textArea = document.querySelector("textarea");
 var outputDiv = document.querySelector("#output");
 
-function clickHandler(){
-    outputDiv.innerText = "text translated : " + textArea.value;
+// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+ var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationUrl(text) {
+    return serverUrl + "?" + "text=" + text
+}
+
+
+function errorHandler(error){
+console.log("error occured" , error)
+console.log("There is an error occured , please try again after sometime")
+}
+
+
+function clickHandler() {
+    // outputDiv.innerText = "text translated : " + textArea.value;
+    var textInput = textArea.value;             // taking input
+
+    //calling server for    process
+    fetch(getTranslationUrl(textInput))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerHTML = translatedText;    //output
+            })
+        .catch( errorHandler);
 };
 
-btnTranslate.addEventListener("click",clickHandler);
+btnTranslate.addEventListener("click", clickHandler);
